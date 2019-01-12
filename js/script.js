43610786a983b46a89b012e583e3bfe80eda1be1
Shimizu-Sunto <46141866+Shimizu-Sunto@users.noreply.github.com>
2019-01-12 15:08:00 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -468,7 +468,7 @@ $(function() {
         var area_master_select_form = $("#select_area_master");
         var select_master_html = "";
 
-        select_master_html += '<option value="-1">地区を選択してください</option>';
+        select_master_html += '<option value="-1">Please select your district.</option>';
         for (var row_index in areaMasterModels) {
           var area_master_name = areaMasterModels[row_index].name;
           var selected = (selected_master_name == area_master_name) ? 'selected="selected"' : "";
