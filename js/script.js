e425e6426500b120b8c57f8615d2fe768c301162
Shimizu-Sunto <46141866+Shimizu-Sunto@users.noreply.github.com>
2019-01-21 17:02:45 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -94,84 +94,13 @@ var TrashModel = function(_lable, _cell, remarks) {
 
   var result_text = "";
   var today = new Date();
-  var wk;
-  var wkNum;
 
   for (var j in this.dayCell) {
-  
-    //曜日を英語に変換
-    if(this.dayCell[j] == "月"){
-    	wk = "Monday";
-    }
-    else if(this.dayCell[j] == "火"){
-    	wk = "Tuesday";
-    }
-    else if(this.dayCell[j] == "水"){
-    	wk = "Wednesday";
-    }
-    else if(this.dayCell[j] == "木"){
-    	wk = "Thursday";
-    }
-    else if(this.dayCell[j] == "金"){
-    	wk = "Friday";
-    }
-    else if(this.dayCell[j] == "土"){
-    	wk = "Saturday";
-    }
-    else if(this.dayCell[j] == "日"){
-    	wk = "Sunday";
-    }
-    else{}
-    
+
     if (this.dayCell[j].length == 1) {
-    
-      result_text += "On " + wk + "s&emsp;";
-      
+      result_text += "On" + this.dayCell[j] + "s";
     } else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) != "*") {
-      
-      //月２の「２」を英語にする
-      if(this.dayCell[j].charAt(1) == "1"){
-      	wkNum = "first ";
-      }
-      else if(this.dayCell[j].charAt(1) == "2"){
-      	wkNum = "second ";
-      }
-      else if(this.dayCell[j].charAt(1) == "3"){
-      	wkNum = "third ";
-      }
-      else if(this.dayCell[j].charAt(1) == "4"){
-      	wkNum = "four ";
-      }
-      else if(this.dayCell[j].charAt(1) == "5"){
-      	wkNum = "five ";
-      }
-      else{}
-      
-      //月２の「月」を英語にする
-      if(this.dayCell[j].charAt(0) == "月"){
-      	wk = "Monday";
-      }
-      else if(this.dayCell[j].charAt(0) == "火"){
-      	wk = "Tuesday";
-      }
-      else if(this.dayCell[j].charAt(0) == "水"){
-      	wk = "Wednesday";
-      }
-      else if(this.dayCell[j].charAt(0) == "木"){
-      	wk = "Thursday";
-      }
-      else if(this.dayCell[j].charAt(0) == "金"){
-      	wk = "Friday";
-      }
-      else if(this.dayCell[j].charAt(0) == "土"){
-      	wk = "Saturday";
-      }
-      else if(this.dayCell[j].charAt(0) == "日"){
-      	wk = "Sunday";
-      }
-      else{}
-      
-      result_text += wkNum + wk + "&emsp;" ;
+      result_text += "第" + this.dayCell[j].charAt(1) + this.dayCell[j].charAt(0) + "曜日 ";
     } else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) == "*") {
     } else if (this.dayCell[j].length == 10 && this.dayCell[j].substr(0,1) == "隔") {
       /**** MOD: PICK biweek, Ex:隔月20140401 ****/
@@ -184,7 +113,7 @@ var TrashModel = function(_lable, _cell, remarks) {
       this.regularFlg = 3;	// 4週フラグ
     } else {
       // 不定期回収の場合（YYYYMMDD指定）
-      result_text = "Irregular ";
+      result_text = "不定期 ";
       this.regularFlg = 0;  // 定期回収フラグオフ
     }
   }
@@ -727,7 +656,7 @@ $(function() {
             } else if (leftDay == 1) {
               leftDayText = "Tomorrow";
             } else {
-              leftDayText = leftDay + "&nbsp;days after";
+              leftDayText = leftDay + "日後";
             }
 	  }
           styleHTML += '#accordion-group' + d_no + '{background-color:  ' + description.background + ';} ';
